8bddc53a57f1a5c94520bea554717423cddc37dd
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-07 20:17:02 -0300

    make it better

--- app.js
+++ app.js
@@ -17,27 +17,8 @@ const corsOptions ={
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.post("/action", function(req,res){
-    res.header('Access-Control-Allow-Origin', '*');
     res.send("Sending notification to a topic...")
     const data = {
         topic: req.body.topic,
@@ -50,31 +31,26 @@ app.post("/action", function(req,res){
 })
 
 app.get('/level', async function(req,res){
-  res.header('Access-Control-Allow-Origin', '*');
   res.send(await db.getAll())
   
 })
 
 
 app.get('/level/:id', async function(req,res){
-  res.header('Access-Control-Allow-Origin', '*');
   res.send(await db.getLevel(req.params.id))
 })
 
 app.post('/level/:id', async function(req,res){
-  res.header('Access-Control-Allow-Origin', '*');
   await db.setRemember(req.params.id,req.body)
   res.send("level "+req.params+" updated")
 })
 
 
 app.get('/level/remember/:id', async function(req,res){
-  res.header('Access-Control-Allow-Origin', '*');
   res.send(await db.getRemember(req.params.id))
 })
 
 app.post('/result', async function(req,res){
-  res.header('Access-Control-Allow-Origin', '*');
   res.send(await db.createResult(req.body))
 })
 
