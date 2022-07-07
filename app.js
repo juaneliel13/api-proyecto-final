f21eb03a91a6959bd43497a01ae03e7d14b58704
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-07 20:49:24 -0300

    make it better

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
- // Use this after the variable declaration
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:false,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.post("/action",cors(corsOptions), function(req,res){
     res.header('Access-Control-Allow-Origin', '*');
