825c6cf0c32c4e4f3f10747db94b0362fde82e03
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-07 21:08:49 -0300

    make it better

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 const cors=require("cors");
 const corsOptions ={
-   origin:'*', 
+   origin:'https://app-viviant.herokuapp.com', 
    credentials:false,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
