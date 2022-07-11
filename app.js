ee42815a7950ab0086a3f568c6292f0e2d5effeb
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:02:58 -0300

    make it better

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(cors(corsOptions)) // Use this after the variable declaration
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
