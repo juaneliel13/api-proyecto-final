103e03c8f0a0b23ced8595fb3fb9bd7d2e6ce725
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:13:10 -0300

    make it better

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express()
 const cors=require("cors");
 const corsOptions ={
    origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
+   credentials:false,            //access-control-allow-credentials:true
    optionSuccessStatus:204,
 }
 
