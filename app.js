a78053908b596efa909dba20a7247851610e4437
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 19:58:04 -0300

    make it better

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const corsOptions ={
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.post('/result',async function(req,res){
 res.send(await db.createResult(req.body))
