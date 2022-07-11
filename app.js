4fc0d4180ab13ead4e353b6ed3fbf0946c655337
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:01:26 -0300

    make it better

--- app.js
+++ app.js
@@ -15,7 +15,23 @@ const corsOptions ={
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-app.options('/result',cors())
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+
+app.use(allowCrossDomain);
 
 app.post('/result',async function(req,res){
 res.send(await db.createResult(req.body))
