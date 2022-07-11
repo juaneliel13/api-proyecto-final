fec4daa6894ad5de3f6727c03a395c32081a4744
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:54:11 -0300

    make it better

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const app = express()
 
 const cors=require("cors");
 const corsOptions ={
-  "origin": "*",
-  "optionsSuccessStatus": 204
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
 }
 
+
 app.use(cors(corsOptions)) // Use this after the variable declaration
+
 
 
 app.post('/result',async function(req,res){
