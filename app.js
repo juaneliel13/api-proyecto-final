28a14d3e2de45fb6a8e332936c34c4c45b60bf99
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:08:36 -0300

    make it better

--- app.js
+++ app.js
@@ -13,8 +13,23 @@ const corsOptions ={
 }
 
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+//app.use(cors(corsOptions)) // Use this after the variable declaration
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 
 
 app.post('/result',async function(req,res){
