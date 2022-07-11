47541ba419a3f5a3f93307466a23c330af672303
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 20:12:01 -0300

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors=require("cors");
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+   optionSuccessStatus:204,
 }
 
 
