452ed1c0d9e60d158c4f843cce54415dcb058e3e
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-11 21:05:22 -0300

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   )
   if (req.method === "OPTIONS") {
     res.header(
