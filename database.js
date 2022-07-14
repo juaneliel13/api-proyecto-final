75027cc2decf51d6b74089359605b403acfb1129
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-14 19:08:44 -0300

    make it better

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-
+const { getFirestore, Timestamp, FieldValue ,addDoc} = require('firebase-admin/firestore');
+import {  addDoc } from "firebase/firestore";
 
 const db = getFirestore();
 
