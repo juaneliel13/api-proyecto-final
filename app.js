679ecb3484c609c7616efeeaf222595844c6c2fb
Juan Eliel Viviant <juanv@unitech.com.ar>
2022-07-14 19:02:18 -0300

    make it better

--- app.js
+++ app.js
@@ -5,27 +5,11 @@ const db = require("./database")
 const app = express()
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
 
 
 
 
 app.post('/result',async function(req,res){
-
 res.send(await db.createResult(req.body))
 })
 
