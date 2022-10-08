9c1126067b260ecca78df60098e36fde28a83a29
Juan Eliel <viviantjuaneliel@gmail.com>
2022-10-08 15:06:08 -0300

    test

--- database.js
+++ database.js
@@ -92,7 +92,7 @@ async function updateLevel(level,shelf,products){
             await db.collection('level').doc(level).collection("shelves").doc(id).set({productos:arr},{merge: true});
         }
     } else {
-        const docRef = await db.collection('level').doc(level).create(await db.collection('shelves'))
+        const docRef = await db.collection('level').doc(level).set({"dificultad":level})
     }
 }
 
