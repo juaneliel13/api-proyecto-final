ae1f347029a4f47c5572805aa5b25793636955a4
Juan Eliel <viviantjuaneliel@gmail.com>
2022-10-08 17:10:07 -0300

    test

--- database.js
+++ database.js
@@ -94,6 +94,7 @@ async function updateLevel(level,shelf,products){
 
 async function existsLevel(level){
     console.log("hola");
+    console.log(await (await db.collection('level').doc(level).get()).exists);
     return await (await db.collection('level').doc(level).get()).exists
 }
 
