23767b91407b0df4aae1a3cdced6265cbe4ef72b
Juan Eliel <viviantjuaneliel@gmail.com>
2022-10-24 22:02:15 -0300

    pruebas setRemember

--- database.js
+++ database.js
@@ -85,7 +85,8 @@ async function updateLevel(level,shelf,products){
     keys.forEach(async e => {
         if( products[e] > 0){
             let cant = await getCurrentProduct(e);
-            console.log("cantidd",e,cant);
+            if(!cant)
+                cant = 1;
             for(let i = 0; i < products[e]; i++)
                 arr.push(e+"-"+cant)
         }
