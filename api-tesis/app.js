7d1238e1ad1bc5d49ed04903580275e6d08a5f43
Juan Eliel <viviantjuaneliel@gmail.com>
2023-04-27 19:54:06 -0300

    updateToRemember corrección

--- api-tesis/app.js
+++ api-tesis/app.js
@@ -1,103 +1,103 @@
-const express = require("express")
-const notification = require("./notifications")
+// const express = require("express")
+// const notification = require("./notifications")
 
-const db = require("./database")
-const app = express()
-
-
-const bodyParser = require('body-parser');
+// const db = require("./database")
+// const app = express()
 
 
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// const bodyParser = require('body-parser');
 
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// // Configurar cabeceras y cors
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 
-
-app.post('/result',async function(req,res){
-  res.send(await db.createResult(req.body))
-})
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 
-app.post("/action", function(req,res){
-  res.send("Sending notification to a topic...")
-  const data = {
-    topic: "config",
-    titulo: req.body.title,
-        mensaje:req.body.body,
-        data:req.body.data
+// app.post('/result',async function(req,res){
+//   res.send(await db.createResult(req.body))
+// })
 
-      }
-      notification.sendPushToTopic(data)
-    })
 
-app.get('/level', async function(req,res){
-  res.send(await db.getAll())
+
+// app.post("/action", function(req,res){
+//   res.send("Sending notification to a topic...")
+//   const data = {
+//     topic: "config",
+//     titulo: req.body.title,
+//         mensaje:req.body.body,
+//         data:req.body.data
+
+//       }
+//       notification.sendPushToTopic(data)
+//     })
+
+// app.get('/level', async function(req,res){
+//   res.send(await db.getAll())
   
-})
+// })
 
 
-app.get('/level/:id', async function(req,res){
-  res.send(await db.getLevel(req.params.id))
-})
+// app.get('/level/:id', async function(req,res){
+//   res.send(await db.getLevel(req.params.id))
+// })
 
-app.post('/level/:id', async function(req,res){
-  await db.setRemember(req.params.id,req.body)
-  res.send("level "+req.params+" updated")
-})
-
-
-app.get('/level/remember/:id', async function(req,res){
-  res.send(await db.getRemember(req.params.id))
-})
-
-app.put('/result/:id', async function(req,res) {
-  res.send(await db.updateResult(req.params.id,req.body))
-})
-
-app.put('/level', async function(req,res) {
-  let index = Object.keys(req.body);
-  let set = new Set([]);
-
-  if(await db.existsLevel(req.query.id)){
-    index.forEach(async e => {
-      await db.updateLevel(req.query.id, e, req.body[e])
-      await db.updateToRemember(req.query.id,req.body, true)
-    });
-  }else{
-    await db.createLevel(req.query.id)
-    index.forEach(async e => {
-      await db.updateLevel(req.query.id, e, req.body[e])
-      await db.updateToRemember(req.query.id,req.body, false)
-    });
-  }
-  res.send()
-})
-
-app.get('/result', async function(req,res) {
-  if(req.query.name)
-    res.send(await db.searchResults(req.query.name))
-})
+// app.post('/level/:id', async function(req,res){
+//   await db.setRemember(req.params.id,req.body)
+//   res.send("level "+req.params+" updated")
+// })
 
 
-app.get('/shelvesConfig', async function(req,res) {
-  res.send(await db.getShelvesConfig())
-})
+// app.get('/level/remember/:id', async function(req,res){
+//   res.send(await db.getRemember(req.params.id))
+// })
+
+// app.put('/result/:id', async function(req,res) {
+//   res.send(await db.updateResult(req.params.id,req.body))
+// })
+
+// app.put('/level', async function(req,res) {
+//   let index = Object.keys(req.body);
+//   let set = new Set([]);
+
+//   if(await db.existsLevel(req.query.id)){
+//     index.forEach(async e => {
+//       await db.updateLevel(req.query.id, e, req.body[e])
+//       await db.updateToRemember(req.query.id,req.body, true)
+//     });
+//   }else{
+//     await db.createLevel(req.query.id)
+//     index.forEach(async e => {
+//       await db.updateLevel(req.query.id, e, req.body[e])
+//       await db.updateToRemember(req.query.id,req.body, false)
+//     });
+//   }
+//   res.send()
+// })
+
+// app.get('/result', async function(req,res) {
+//   if(req.query.name)
+//     res.send(await db.searchResults(req.query.name))
+// })
 
 
-// Start the server
-const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+// app.get('/shelvesConfig', async function(req,res) {
+//   res.send(await db.getShelvesConfig())
+// })
+
+
+// // Start the server
+// const PORT = process.env.PORT || 8081;
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`);
+//   console.log('Press Ctrl+C to quit.');
+// });
